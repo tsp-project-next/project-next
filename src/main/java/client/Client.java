0863fe9853384241e2290a340b9cd318a438e9e1
Logon1027 <logondz27@gmail.com>
2019-02-19 17:32:13 -0500

    - Client constructor reworked - establishObjectStreams method implemented to initialize the client object streams - sendPacket method implemented to send a packet to the server and wait to receive an object in response - closeStreams method implemented to close all streams when the client is exiting the program.

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -1,25 +1,25 @@
 package client;
 
-
 import com.wrapper.spotify.SpotifyApi;
 import com.wrapper.spotify.SpotifyHttpManager;
 import com.wrapper.spotify.requests.authorization.authorization_code.AuthorizationCodeUriRequest;
 
-import java.net.URI;
+import java.net.*;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.net.ConnectException;
-import java.net.Socket;
-import java.net.UnknownHostException;
 
 public class Client {
 
     //Host name or ip address
-    String host = "localhost";
+    private String host = "localhost";
 
     //Socket for establishing connection
-    Socket socket;
+    private Socket socket;
+    //Object output stream to the server
+    private ObjectOutputStream toServer;
+    //Object input stream from the server
+    private ObjectInputStream fromServer;
 
     // Can be moved later depending on how we want to fully implement this
     private static final String clientId = "ef5f89735e4649929f4e9eb8fac2db06";
@@ -45,50 +45,32 @@ public class Client {
         System.out.println("This is a successful run after gradle build...");
 
         authorizationCodeUri_Sync();
-        new Client();
+
+        Client client = new Client();
+        client.closeStreams();
     }
 
     public Client() {
-        try {
-            System.out.println("client created...");
+        System.out.println("client created...");
 
-            //Establish connection with server
-            if(establishConnection(host) == false) {
-                return;
-            }
-            System.out.println("Is socket connected: " + socket.isConnected());
-
-            //Output stream has to be created before input stream.
-
-            //Create and object output stream to the server
-            ObjectOutputStream toServer = new ObjectOutputStream(socket.getOutputStream());
-
-            //Create and object input stream to the server
-            ObjectInputStream fromServer = new ObjectInputStream(socket.getInputStream());
-
-            System.out.println("Streams established");
-
-            //Create a packet to send
-            Packet packet = new Packet("Testing", "host");
-            toServer.writeObject(packet);
-            System.out.println("wrote object...");
-
-            try {
-                Object obj = fromServer.readObject();
-                Packet received = (Packet) obj;
-                System.out.println("Successfully read object: " + received.getData());
-            } catch (ClassNotFoundException ex) {
-                ex.printStackTrace();
-            }
-
-        } catch(IOException ex) {
-            ex.printStackTrace();
+        //Establish connection with server
+        if(establishConnection(host) == false) {
+            return;
         }
+
+        //establish object data streams with server
+        if(establishObjectStreams(socket) == false) {
+            return;
+        }
+
+        //Send a test packet to the server
+        Packet received = sendPacket("Testing", "host");
     }
 
-    public boolean establishConnection(String host) {
+    private boolean establishConnection(String host) {
         try {
             socket = new Socket(host, 9000);
+            System.out.println("Socket connected");
             return true;
         } catch (ConnectException ex) {
             System.out.println("No server connection found.");
@@ -98,6 +80,60 @@ public class Client {
             ex.printStackTrace();
         }
         return false;
+    }
+
+    private boolean establishObjectStreams(Socket socket) {
+        try {
+            //Output stream has to be created before input stream.
+            //Create and object output stream to the server
+            toServer = new ObjectOutputStream(socket.getOutputStream());
+            //Create and object input stream to the server
+            fromServer = new ObjectInputStream(socket.getInputStream());
+            System.out.println("Streams established");
+            return true;
+        } catch(IOException ex) {
+            ex.printStackTrace();
+            System.out.println("failed to establish object streams");
+            return false;
+        }
+    }
+
+    private Packet sendPacket(String data, String cid) {
+        try {
+            //Create a packet to send
+            Packet packet = new Packet(data, cid);
+            toServer.writeObject(packet);
+            System.out.println("Object sent to server...");
+
+            try {
+                //wait for the object response from the server
+                Object obj = fromServer.readObject();
+                Packet received = (Packet) obj;
+                System.out.println("Successfully read object: " + received.getData());
+                return received;
+            } catch(ClassNotFoundException ex) {
+                ex.printStackTrace();
+            } catch(SocketException ex) {
+                System.out.println("Connection to server reset/lost");
+                ex.printStackTrace();
+            }
+            return null;
+        } catch(IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    }
+
+    private void closeStreams() {
+        try {
+            System.out.println("Attempting to close streams...");
+            if(fromServer != null) fromServer.close();
+            if(toServer != null) toServer.close();
+            if(socket != null) socket.close();
+            System.out.println("Streams closed.");
+        } catch(IOException ex) {
+            ex.printStackTrace();
+        }
     }
 
     public String fakeTest()
