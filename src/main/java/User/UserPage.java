91148b833b7e785fe5ce41e0712ee2b2246f3346
theomars115 <eyewakey@gmail.com>
2019-02-22 16:12:00 -0500

    -RGB Code for Pages and User page added

--- src/main/java/User/UserPage.java
+++ src/main/java/User/UserPage.java
@@ -1,21 +1,20 @@
 package User;
 
-import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
-import javafx.stage.Stage;
 
-public class LandingPage {
-    private static Scene landingScene = null;
-    public LandingPage() {
+public class UserPage {
+
+    private static Scene UserScene = null;
+    public UserPage() {
         //filler code at the moment
         StackPane root = new StackPane();
-        landingScene = new Scene(root, 300, 200, Color.rgb(		26, 83, 46));
+        UserScene = new Scene(root, 300, 200, Color.FORESTGREEN);
     }
 
     //returns the scene to be used on the current window
     public Scene getScene() {
-        return landingScene;
+        return UserScene;
     }
 }
